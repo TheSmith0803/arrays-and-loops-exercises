71e42f8a6b0e7fe4633c587fa0a5f368b7bda275
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-19 21:53:02 -0500

    it was much simpler than I was making it

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,13 +1,5 @@
 const reverseString = function(user_input) {
-    let words_arr = user_input.split(' ').reverse();
-
-    for (let i = 0; i < words_arr.length; i++) {
-        words_arr[i] = words_arr[i].split('').reverse().join().replaceAll(',', '');
-    }
-    
-    words_arr = words_arr.join(' ');
-
-    return words_arr;
+    return user_input.split('').reverse().join('');
 };
 
 // Do not edit below this line
