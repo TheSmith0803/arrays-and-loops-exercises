3a1505c8439a4cb4d6ea25fd8c19991fe9016514
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-19 21:47:30 -0500

    solution 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,13 +1,16 @@
-const reverseString = function(string) {
-    let words_arr = string.split(' ').reverse();
+const reverseString = function(user_input) {
+    let words_arr = user_input.split(' ').reverse();
 
-    for (let i in words_arr) {
-        i.split('').reverse().join();
-        console.log(i);
+    for (let i = 0; i < words_arr.length; i++) {
+        words_arr[i] = words_arr[i].split('').reverse().join().replaceAll(',', '');
     }
+    
+    words_arr = words_arr.join(' ').replaceAll(',', '');
 
     return words_arr;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
