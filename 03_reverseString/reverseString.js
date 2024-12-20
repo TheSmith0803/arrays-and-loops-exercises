09f35a59ea7ea38c027adee93a713a5b60207e1c
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-19 21:27:44 -0500

    trying to reverse individual words in strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let words_arr = string.split(' ').reverse();
 
+    for (let i in words_arr) {
+        i.split('').reverse().join();
+        console.log(i);
+    }
+
+    return words_arr;
 };
 
 // Do not edit below this line
